86ac12b408cb1fdac320f2b13ffdcfe905e73b8e
Magdalena <cmagi829@gmail.com>
2022-08-23 18:07:39 -0500

    Initial Commit

---
+++ practica3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practica3
+//
+//  Created by user224984 on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
